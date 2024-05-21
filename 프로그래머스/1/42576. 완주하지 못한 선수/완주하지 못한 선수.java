d3acf726c64428e827c669bd656b918e7e80d008
yezy <82196217+yezyaa@users.noreply.github.com>
2024-05-21 23:33:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.33 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,26 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 1. 두 배열을 정렬한다.
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        String answer = "";
         
-        // 2. 두 배열이 다를 때까지 찾는다.
-        int i = 0; // 마지막 주자가 완주하지 못했을 경우를 위해 변수를 따로 작성했다.
-        for (; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i]))
+         // 1. HashMap 생성 (Participant)
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant)
+            map.put(player, map.getOrDefault(player, 0) + 1);
+
+        // 2. HashMap 제거 (Completion)
+        for (String player : completion)
+            map.put(player, map.get(player) - 1);
+
+        // 3. value가 0이 아닌 마지막 주자 찾기
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer  = key;
                 break;
+            }
         }
         
-        // 3. 결과 확인      
-        return participant[i];
+        return answer;
     }
 }
