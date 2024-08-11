ded8770a920a696d2db20694b113aa949e14a200
yezy <82196217+yezyaa@users.noreply.github.com>
2024-08-11 13:19:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
         int answer = 0;
+        long longNum = num;
 
-        if (num == 1)
-            return 0;
-
-        while (num != 1) {
-            if (num % 2 == 0)
-                num /= 2;
+        while (longNum != 1) {
+            if (longNum % 2 == 0)
+                longNum /= 2;
             else
-                num = num * 3 + 1;
+                longNum = longNum * 3 + 1;
             answer++;
 
             if (answer >= 500)
