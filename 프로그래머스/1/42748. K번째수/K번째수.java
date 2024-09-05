5dff1bad4717c84bfde60ec3f107ebdfeab55b8c
yezy <82196217+yezyaa@users.noreply.github.com>
2024-09-05 23:44:22 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,20 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // commands 길이 만큼의 결과 배열 생성
         int[] answer = new int[commands.length];
-        
+
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int k = commands[i][2];
-            
-            int[] subArray = Arrays.copyOfRange(array, start - 1, end);
-            
-            Arrays.sort(subArray); // 오름차순 정렬
-            
-            answer[i] = subArray[k - 1];
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
+
         return answer;
     }
 }
